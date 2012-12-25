1824f102643bd686844b2b3b58af764e2efa2480
JinTu <jintu@praecogito.com>
2012-12-25 13:49:38 -0800

    Fixed name reference in src/aerocli5.h

--- src/aerocli5.h
+++ src/aerocli5.h
@@ -1,6 +1,6 @@
 /* Copyright 2012-2013 lynix <lynix47@gmail.com>
  *
- * This file is part of aerotools.
+ * This file is part of aerotools-ng.
  *
  * aerotools-ng is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
