aadad04448be078a04320d3edd2a493da43d0acc
Alex Deymo <deymo@chromium.org>
2013-07-18 10:11:32 -0700

    libaquaero: Make local functions static.
    This prevents those symbols, not exported in the libaquaero5.h header,
    to conflict with other programs.

--- src/libaquaero5.c
+++ src/libaquaero5.c
@@ -69,13 +69,13 @@ int aq5_fd = -1;
 
 /* helper functions */
 
-inline uint16_t aq5_get_int16(unsigned char *buffer, short offset)
+static inline uint16_t aq5_get_int16(unsigned char *buffer, short offset)
 {
 	return (uint16_t)((buffer[offset] << 8) | buffer[offset + 1]);
 }
 
 
-inline uint32_t aq5_get_int32(unsigned char *buffer, short offset)
+static inline uint32_t aq5_get_int32(unsigned char *buffer, short offset)
 {
 	return (buffer[offset] << 24) | (buffer[offset + 1] << 16) |
 			(buffer[offset + 2] << 8) | buffer[offset + 3];
@@ -83,7 +83,7 @@ inline uint32_t aq5_get_int32(unsigned char *buffer, short offset)
 
 
 /* get the uptime for the given value in seconds */
-inline void aq5_get_uptime(uint32_t timeval, struct tm *uptime)
+static inline void aq5_get_uptime(uint32_t timeval, struct tm *uptime)
 {
 	uptime->tm_sec = timeval;
 	uptime->tm_min = uptime->tm_sec / 60;
@@ -98,7 +98,7 @@ inline void aq5_get_uptime(uint32_t timeval, struct tm *uptime)
 }
 
 
-inline void aq5_get_time(uint32_t timeval, struct tm *time)
+static inline void aq5_get_time(uint32_t timeval, struct tm *time)
 {
 	time->tm_min = 0;
 	time->tm_hour = 0;
@@ -111,7 +111,7 @@ inline void aq5_get_time(uint32_t timeval, struct tm *time)
 }
 
 
-char *aq5_strcat(char *str1, char *str2)
+static char *aq5_strcat(char *str1, char *str2)
 {
 	char *ret;
 
@@ -123,7 +123,7 @@ char *aq5_strcat(char *str1, char *str2)
 	return ret;
 }
 
-int aq5_open(char *device, char **err_msg)
+static int aq5_open(char *device, char **err_msg)
 {
 	struct hiddev_devinfo dinfo;
 
@@ -225,7 +225,7 @@ int aq5_open(char *device, char **err_msg)
 
 
 /* Get the specified HID report */
-int aq5_get_report(int fd, int report_id, unsigned report_type, unsigned char *report_data)
+static int aq5_get_report(int fd, int report_id, unsigned report_type, unsigned char *report_data)
 {
 	struct hiddev_report_info rinfo;
 	struct hiddev_field_info finfo;
